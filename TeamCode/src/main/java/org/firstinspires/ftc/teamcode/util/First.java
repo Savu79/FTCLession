c7e68d0135c2e8c124f4d49d77c92d18188acd23
Savu79 <vladsavulescupana@gmail.com>
2024-09-28 22:09:26 +0300

    second commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/First.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/First.java
@@ -12,17 +12,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class First extends LinearOpMode {
 
     DcMotorEx motor1;
+    DcMotorEx motor;
     Servo servo1;
+    boolean bpress = true;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         servo1=hardwareMap.get(Servo.class, "servo1");
+        servo1.setPosition(0);
         motor1= hardwareMap.get(DcMotorEx.class, "motor1");
+        motor.setPower(1);
 
         waitForStart();
         while (opModeIsActive()) {
-
+            if(gamepad1.a){
+                if(bpress){
+                    servo1.setPosition(1);
+                    bpress = false;
+                    sleep(1000);
+                }
+                else {
+                    servo1.setPosition(0);
+                    bpress = true;
+                    sleep(1000);
+                }
+            }
         }
     }
 }
