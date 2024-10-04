1a58c0862c56e687a3ac38d7ff29976799592ab4
Savu79 <vladsavulescupana@gmail.com>
2024-10-04 16:11:45 +0300

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/First.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/First.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode.util;
 
-
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -12,17 +12,20 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class First extends LinearOpMode {
 
     DcMotorEx motor1;
-    DcMotorEx motor;
+    DcMotorEx motor2;
     Servo servo1;
     boolean bpress = true;
+    double dpress=0;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         servo1=hardwareMap.get(Servo.class, "servo1");
         servo1.setPosition(0);
-        motor1= hardwareMap.get(DcMotorEx.class, "motor1");
-        motor.setPower(1);
+        motor1=hardwareMap.get(DcMotorEx.class, "motor1");
+        motor1.setPower(0);
+        motor2=hardwareMap.get(DcMotorEx.class, "motor2");
+
 
         waitForStart();
         while (opModeIsActive()) {
@@ -38,6 +41,17 @@ public class First extends LinearOpMode {
                     sleep(1000);
                 }
             }
+            motor1.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+            motor2.setPower(dpress);
+            if(gamepad1.dpad_right)
+            {
+                dpress=1;
+            } else if (gamepad1.dpad_left) {
+                dpress=-1;
+            } else if (gamepad1.dpad_down) {
+                dpress=0;
+            }
+
         }
     }
 }
